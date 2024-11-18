2207258fefeac51051aa584ba8a17d73f8a0eb64
ROYAL-SCOT <181339098+R0YAL-SCOT@users.noreply.github.com>
2024-11-18 17:28:03 +0000

    nmap stage mostly complete, need to work on OpenVAS and Metasploit

--- MainToolkit.py
+++ MainToolkit.py
@@ -9,14 +9,20 @@ import os
 def main():
 
 
+    #user input using params
+
     nmap()
+    openVAS()
     metasploit()
 
     
 
 
 def nmap():
-    runNmap='nmap --open -A -Pn -sT -T'+str()+' '+str()+' -oX '+chr(34)++chr(34)
+    #using script vuln to get CVE codes, use codes to find auxillary scanners in Metasploit
+    #speed, ip address and file type
+    #might be too much information in nmap scan
+    runNmap='nmap --script vuln --open -A -Pn -sT -T'+str()+' '+str()+' -oX '+chr(34)++chr(34)
 
     print(runNmap)
     exit_code = os.system(runNmap)
@@ -27,11 +33,24 @@ def nmap():
 
     print("Info: Output can be found in " + filename)
 
+def openVAS():
+
+    runOpenVAS=''
+
+
+
+
 
 def metasploit():
+
+    #start with finding nmap file and using reg ex to find CVE codes
+    #launch msfconsole and do 'search cve:<CVE_code_here>'
+
+
     #start rpc server with standard username and password and set deamon to foreground
     runmsfrpcd='msfrpcd -U user -P password -a 127.0.0.1 -f'
     #connect to rpc server on localhost and use previous set username and password
     runmsfrpc='msfrpc -U user -P password -a 127.0.0.1'
     #run the auxillary PoC scanner using the input data from the user
     runMSF='rpc.call("module.execute", "auxillary", "put scanner here", {"put required data here, host, port, etc"})'
+    
