718d76738d32980a9be627c14a7fbe402cf1f875
R0YAL-SCOT <181339098+R0YAL-SCOT@users.noreply.github.com>
2025-01-27 11:32:33 +0000

    Creating a gvm task

--- MainToolkit.py
+++ MainToolkit.py
@@ -59,6 +59,7 @@ def gvm(ipaddress, filename):
     os.system('gvm start')
 
     #create task and start
+    os.system('gvm-cli tls --gmp-username admin --gmp-password admin --xml "<create_task><name>3000</name>> <target target_id=""></target><scanner id="08b69003-5fc2-4037-a479-93b440211c73"><name>Vulnerability Scan</name><type>2</type></create_task>" --host 127.0.0.1')
     
     #use a script to run a certain script for vulnerability scan
     os.system('gvm-script --gmp-username name --gmp-password pass ssh --hostname <gsm> scripts/start-nvt-scan.gmp.py <oid of nvt> <target>')
