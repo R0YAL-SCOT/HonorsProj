fb9a3b01a1bd8420bf8ee34c90c90abff216a34e
R0YAL-SCOT <181339098+R0YAL-SCOT@users.noreply.github.com>
2025-01-27 15:06:08 +0000

    new gvm connection script for python-gvm. Used to access gvm tasks and scans

---
+++ gvmConnectionScript.py
@@ -0,0 +1,27 @@
+#python3 -m pip install --user python-gvm
+
+from gvm.connections import UnixSocketConnection
+from gvm.protocols.gmp import Gmp
+from gvm.transforms import EtreeTransform
+from gvm.xml import pretty_print
+
+connection = UnixSocketConnection()
+transform = EtreeTransform()
+
+with Gmp(connection, transform=transform) as gmp:
+    # Retrieve GMP version supported by the remote daemon
+    version = gmp.get_version()
+
+    # Prints the XML in beautiful form
+    pretty_print(version)
+
+    # Login
+    gmp.authenticate('foo', 'bar')
+
+    # Retrieve all tasks
+    tasks = gmp.get_tasks()
+    gmp.get_scanner( 'vuln scan', '127.0.0.1', 9390, scanner_type=2 , credential_id='up')
+
+    # Get names of tasks
+    task_names = tasks.xpath('task/name/text()')
+    pretty_print(task_names)
