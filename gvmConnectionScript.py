6f31c2fc5d3ca044da162b25f0262169bc8a6fa3
R0YAL-SCOT <181339098+R0YAL-SCOT@users.noreply.github.com>
2025-01-27 15:22:48 +0000

    cleaning up gvm connection file

--- gvmConnectionScript.py
+++ gvmConnectionScript.py
@@ -5,23 +5,29 @@ from gvm.protocols.gmp import Gmp
 from gvm.transforms import EtreeTransform
 from gvm.xml import pretty_print
 
-connection = UnixSocketConnection()
-transform = EtreeTransform()
 
-with Gmp(connection, transform=transform) as gmp:
-    # Retrieve GMP version supported by the remote daemon
-    version = gmp.get_version()
+def connection():
+    connection = UnixSocketConnection()
+    transform = EtreeTransform()
 
-    # Prints the XML in beautiful form
-    pretty_print(version)
+    with Gmp(connection, transform=transform) as gmp:
+        # Retrieve GMP version supported by the remote daemon
+        version = gmp.get_version()
 
-    # Login
-    gmp.authenticate('foo', 'bar')
+        # Prints the XML in beautiful form
+        pretty_print(version)
 
-    # Retrieve all tasks
-    tasks = gmp.get_tasks()
-    gmp.get_scanner( 'vuln scan', '127.0.0.1', 9390, scanner_type=2 , credential_id='up')
+        # Login
+        gmp.authenticate('foo', 'bar')
 
-    # Get names of tasks
-    task_names = tasks.xpath('task/name/text()')
-    pretty_print(task_names)
+        # Retrieve all tasks
+        tasks = gmp.get_tasks()
+        gmp.get_scanner( 'vuln scan', '127.0.0.1', 9390, scanner_type=2 , credential_id='up')
+
+        # Get names of tasks
+        task_names = tasks.xpath('task/name/text()')
+        pretty_print(task_names)
+
+
+if __name__=="__main__":
+    connection()
